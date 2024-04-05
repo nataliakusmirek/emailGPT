11b1cc42e2f6961fb8125341abc5daa8fcdb2953
Natalia Kusmirek <nekusmirek@wpi.edu>
2024-04-05 12:15:07 -0400

    starter code complete

---
+++ train.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+batch_size = 16 # how many independent sequences will we process in parallel?
+block_size = 32 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 100
+learning_rate = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 64
+n_head = 4
+n_layer = 4
+dropout = 0.0
+
+torch.manual_seed(1337)
+
+# Get the data of the training set and train the model
+!wget https://raw.githubusercontent.com/nataliakusmirek/emailGPT/main/training.txt?token=GHSAT0AAAAAACPNHDIVAP2LX6O556RTKHMCZQQFM3A
+
+# Read in to inspect
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# Here are all the unique characters of this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+# Create a mapping from character to integers, aka. TOKENIZE
+char_to_int = {c: i for i, c in enumerate(chars)}
+int_to_char = {i: c for i, c in enumerate(chars)}
+encode = lambda x: [char_to_int[c] for c in x] # encoder: take a string, output a list
+decode = lambda x: ''.join([int_to_char[c] for c in x]) # decoder: take a list, output a string
+
+
+# Tokenize the entire training set
+data = torch.tensor(encode(text), dtype=torch.long) # the characters of our dataset have been converted to integers, and stored in a tensor called data.
+# Split data into training and validation sets
+n = int(0.9 * len(data)) # first 90% of the data will be used for training, remaining 10% for validation
+train_data, val_data = data[:n], data[n:]
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size, ))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+
+# Figure out average loss for training and validation sets
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
+
+print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
